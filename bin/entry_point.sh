2aecb9c69d65568d90121b90b6c6852f16c6d6ba
AussieK <yallsung85@gmail.com>
2025-07-22 10:06:23 +0900

    add push

--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -1,22 +1,57 @@
 #!/bin/bash
 
-CONFIG_FILE=_config.yml 
+# CONFIG_FILE=_config.yml 
 
-/bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
+# /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
 
-while true; do
+# while true; do
 
-  inotifywait -q -e modify,move,create,delete $CONFIG_FILE
+#   inotifywait -q -e modify,move,create,delete $CONFIG_FILE
 
-  if [ $? -eq 0 ]; then
+#   if [ $? -eq 0 ]; then
  
+#     echo "Change detected to $CONFIG_FILE, restarting Jekyll"
+
+#     jekyll_pid=$(pgrep -f jekyll)
+#     kill -KILL $jekyll_pid
+
+#     /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
+
+#   fi
+
+# done
+
+CONFIG_FILE=_config.yml
+
+# Start Jekyll with live reload and watch enabled
+bundle exec jekyll serve \
+  --watch \
+  --port=8080 \
+  --host=0.0.0.0 \
+  --livereload \
+  --incremental \
+  --force_polling \
+  --verbose \
+  --trace &
+jekyll_pid=$!
+
+# Only restart if _config.yml changes
+while true; do
+  inotifywait -q -e modify,move,create,delete $CONFIG_FILE
+  if [ $? -eq 0 ]; then
     echo "Change detected to $CONFIG_FILE, restarting Jekyll"
 
-    jekyll_pid=$(pgrep -f jekyll)
     kill -KILL $jekyll_pid
 
-    /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
-
+    bundle exec jekyll serve \
+      --watch \
+      --port=8080 \
+      --host=0.0.0.0 \
+      --livereload \
+      --incremental \
+      --force_polling \
+      --verbose \
+      --trace &
+    jekyll_pid=$!
   fi
-
 done
